76fb0a7c8de85b8749d8611a91d0b68dfa51792a
zisam-glitch <128255158+zisam-glitch@users.noreply.github.com>
2023-05-18 22:33:36 +0600

    first commit

---
+++ src/components/menu/main-menu/index.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const MainMenu = () => {
+    return (
+        <nav>
+            <ul className="main-menu">
+                <li className="active">
+                    <NavLink className="main-menu-link" exact to="/">
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        className="main-menu-link"
+                        to={process.env.PUBLIC_URL + "/service"}
+                    >
+                        Services
+                    </NavLink>
+                    <ul className="sub-menu">
+                        <li>
+                            <NavLink
+                                className="sub-menu-link"
+                                to={process.env.PUBLIC_URL + "/service"}
+                            >
+                                Services
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className="sub-menu-link"
+                                to={
+                                    process.env.PUBLIC_URL +
+                                    "/service-details/1"
+                                }
+                            >
+                                Service Details
+                            </NavLink>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <NavLink
+                        className="main-menu-link"
+                        to={process.env.PUBLIC_URL + "/blog"}
+                    >
+                        Blog
+                    </NavLink>
+                    <ul className="sub-menu">
+                        <li>
+                            <NavLink
+                                className="sub-menu-link"
+                                to={process.env.PUBLIC_URL + "/blog"}
+                            >
+                                Blog list
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className="sub-menu-link"
+                                to={process.env.PUBLIC_URL + "/blog-details/1"}
+                            >
+                                Blog Details
+                            </NavLink>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <NavLink
+                        className="main-menu-link"
+                        to={process.env.PUBLIC_URL + "/about"}
+                    >
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        className="main-menu-link"
+                        to={process.env.PUBLIC_URL + "/contact"}
+                    >
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default MainMenu;
